9d69b36c483e9c3d604a14d5485a403849f7fc11
meizigz <131223081+meizigz@users.noreply.github.com>
2023-08-14 19:50:49 +0800

    Update 009_set_map.dart
    增加遍历的示例

--- 001_dart_basics/009_set_map.dart
+++ 001_dart_basics/009_set_map.dart
@@ -12,6 +12,15 @@ void main() {
   gifts['third'] = 'rose'; // 映射设置key的值，如果key尚未存在则新增
   var g = gifts['fourth']; // 取值。推断为String?，如果对应的值不存在则返回null
 
+  // 遍历Map
+  gifts.entries.forEach((element) {
+    print(element.key);
+    print(element.value);
+  });
+
+  gifts.remove('fifth'); // 删除元素。返回删除的元素
+  gifts.clear(); // 清空
+
   var names = <String>{}; // 空Set。Set<String>，集合，元素无关系
   var infos = <String, String>{}; // 空Map。Map<String,String>，映射，k和v
 
