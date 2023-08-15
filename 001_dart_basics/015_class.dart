5f89bb9783020e960feb92d0df93188fff0a4290
meizigz <131223081+meizigz@users.noreply.github.com>
2023-08-15 10:20:13 +0800

    Update 015_class.dart

--- 001_dart_basics/015_class.dart
+++ 001_dart_basics/015_class.dart
@@ -1,55 +1,49 @@
-// class 关键字；因为有抽象方法，所以变成了抽象类
+// class 关键字；
 class Point {
   int? x; // 实例变量，每个实例互相独立
-  int? y; // 每个实例变量都有个隐式的Getter方法来获取值，一般也都还有个隐式Setter方法设置值
-  static const d = 16; // 静态变量。使用类名访问
+  int? y; // 每个实例变量都有隐式的Getter方法获取值，一般也还有隐式Setter方法设置值
+  static const d = 16; // 静态变量。使用类名访问 Point.d
 
-  int get xy => x! + y!; // get 关键字定义了一个可以读的属性xy。!是为了解决空警告
-  set xy(v) => x = v; // set关键字定义了一个可以写的属性
+  // get 关键字定义了一个可以读的属性xy，是运算后得到的，不是直接的变量。
+  int get xy => x! + y!; // n = p.xy  !是为了解决空警告
+  // set关键字定义了一个可以写的属性。p.xy = 10
+  set xy(v) {
+    x = v;
+    y = v;
+  }
 
   // 有显性构造函数则没有默认无参构造函数了
-  Point(this.x, this.y); // 构造函数初始化成员变量，this不能省略
-
-  // 命名构造函数，更容易理解这个构造函数怎么用
-  Point.fromJson(Map<String, int> a) {
-    x = a['x']; // 如果和局部变量名不冲突，可以省略this
-    this.y = a['y'];
+  Point(int x, int y) {
+    this.x = x; // this关键字区分开了同名变量
+    this.y = y; // 初始值保存到变量。有简便写法，见下个例子
   }
-  // 调用已有的构造函数（复用代码）
-  Point.oneParam(int v) : this(v, 0);
-  // 构造函数不能被子类继承
 }
 
+// 不可变对象类，构造函数是const的即可。相同值的const对象指向同一个地址
 class ImmutablePoint {
-  final int x; // 相同值的const对象指向同一个地址
-  final int y; // final实例变量不能修改值，所以没有隐式的Setter方法
-  const ImmutablePoint(this.x, this.y); // const 构造函数要求所有成员都必须是final的
-  const ImmutablePoint.unnamed()
-      : x = 0, // 也可以用初始化列表初始值。会在函数体之前执行
-        y = 0; // ','分割
-  // 如果是 late final，那么就要通过方法明确设置值
+  final int x; // const 构造函数要求所有字段都为final的，初始值后不能再修改
+  final int y; // 这样才符合语义上的const对象始终不变
+  const ImmutablePoint(int x, int y) // const构造函数不能有函数体
+      : this.x = x, // 属性要在初始化列表中赋值
+        this.y = y; // 冒号后面加上逗号分隔的语句
+  // const ImmutablePoint(this.x, this.y);  // 也可以写为这种简便写法，对属性初始化
 }
 
-// extends关键字继承；抽象类无法实例化（一般用于继承）
-abstract class ChildP extends Point {
-  int? c;
-  // 父类没有0参数构造函数，所以要明确调用有参父类构造函数，并且要在初始化列表最后调用
-  ChildP()
-      : c = 0,
-        super(0, 0);
-
-  // @override 可以表示重写了一个成员
-
+// abstract抽象类无法实例化（一般用于继承）
+abstract class ChildP {
+  // 继承此类的之类来实现。 @override 注解说明是覆写的父类
   void doSg(); // 没有方法体，抽象方法
 }
 
 void main() {
   Point? p;
   p = Point(2, 3); // 构造新对象。可以不用new关键字
+  print(p.runtimeType); // Type 获取对象运行时的实际类型
+
   int? n;
   n = p.x; // 访问成员变量
+  print(n);
   // n = p?.y; // 如果p不为空则返回y成员变量；否则返回null
-  p = Point.fromJson({'x': 4, 'y': 5}); // 用标识符构造函数创建新对象
   const cp = const ImmutablePoint(2, 2); // const 对象只能由const构造函数生成
-  print(p.runtimeType); // Type 获取对象运行时的实际类型
+  print(cp);
 }
