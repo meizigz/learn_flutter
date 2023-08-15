e5caff06fbf5cd2ce4d5cb2f6a4d16911f60cc83
meizigz <131223081+meizigz@users.noreply.github.com>
2023-08-15 15:41:13 +0800

    Update main.dart

--- 002_first_app/lib/main.dart
+++ 002_first_app/lib/main.dart
@@ -1,30 +1,29 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp()); // 调用flutter框架的runApp启动App（需要一个widget作为参数）
+  runApp(MyApp());
 }
 
-// 继承自无状态Widget，没有私有的会变的状态
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // 覆写build方法。创建widget后，flutter框架以context作为参数来调用，以确定此widget的外观。
   @override
   Widget build(BuildContext context) {
     // 要返回一个widget；ChangeNotifierProvider用来作为全局状态管理
     return ChangeNotifierProvider(
-      // create属性：一个返回ChangeNotifier的函数，一般都是箭头函数，返回一个自定义的状态对象
+      // create参数：一个返回ChangeNotifier的函数，一般都是箭头函数，返回一个自定义的状态对象
       create: (context) => MyAppState(),
-      // child属性：一个和此Provider绑定的widget，此处是创建了一个MaterialApp
+      // child参数：一个和此Provider绑定的widget，此处是创建了一个MaterialApp
       child: MaterialApp(
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
-        home: MyHomePage(), // 绑定自定义的主界面widget
+        // home参数：绑定自定义的主界面widget
+        home: MyHomePage(),
       ),
     );
   }
@@ -32,7 +31,8 @@ class MyApp extends StatelessWidget {
 
 // ChangeNotifier和ChangeNotifierProvider配套使用
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random(); // 保存状态值的字段
+  // 保存状态值的字段
+  var current = WordPair.random();
 
   // 操作状态值的方法
   void getNext() {
@@ -42,57 +42,21 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
+// 此例中的homepage widget没有私有状态了，所以改成从无状态继承
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 通过context.watch<MyAppState>()获取全局状态对象。
     // PS：也可以用Provider.of<MyModel>(context, listen: true)来获取。当listen为false的时候，只取值不监听。
     var appState = context.watch<MyAppState>();
-    // 获取状态对象中保存的状态值
-    var pair = appState.current;
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('A random AWESOME idea:'),
-            BigCard(pair: pair),
-            ElevatedButton(
-                onPressed: () {
-                  // 调用状态对象中的方法，更新状态值。
-                  appState.getNext();
-                },
-                child: Text('Next')),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final textStyle = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(
-          pair.asLowerCase,
-          style: textStyle,
-        ),
+      body: Column(
+        children: [
+          Text('A random idea:'),
+          // 显示当前状态值
+          Text(appState.current.asLowerCase),
+        ],
       ),
     );
   }
