daa05f3140545eb64b6f576f78f24e9d54e2dd56
meizigz <131223081+meizigz@users.noreply.github.com>
2023-08-27 09:13:59 +0800

    Update main.dart

--- 004_textfield/lib/main.dart
+++ 004_textfield/lib/main.dart
@@ -8,19 +8,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter 文本输入',
+      title: 'TextField Example',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter 文本输入'),
+          title: const Text('TextField Example'),
         ),
         body: Center(
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: '请输入文本',
-            ),
-          ),
+          child: TextFieldExample(),
         ),
       ),
+    );
+  }
+}
+
+class TextFieldExample extends StatefulWidget {
+  @override
+  _TextFieldExampleState createState() => _TextFieldExampleState();
+}
+
+class _TextFieldExampleState extends State<TextFieldExample> {
+  TextEditingController _controller = TextEditingController();
+  String _displayText = '';
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _updateDisplayText(String newText) {
+    setState(() {
+      _displayText = newText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextField(
+          controller: _controller,
+          onChanged: _updateDisplayText,
+          decoration: const InputDecoration(
+            labelText: 'Enter some text',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'You entered: $_displayText',
+          style: const TextStyle(fontSize: 16),
+        ),
+      ],
     );
   }
 }
